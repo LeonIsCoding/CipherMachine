e56fa59ccadeb55f3bf6fcead3ed456a49b1bd25
lovelyleon <lovelylayon@gmail.com>
2019-03-03 19:38:08 +0000

    Removed Eclipse related files.

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-    	CipherMachine cm = new CipherMachine();
-    	
-    	System.out.println(cm.ElonDecoder("Ugfyjslmdslagfk ]]] Lwdd fgtgvq lzw hskkogjv ak! ,Uzwvvsj,", 18));
-    	System.out.println(cm.ElonEncoder("HelloHello", 3));
-    }
-}
